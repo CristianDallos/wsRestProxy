5c08cb0f9f8e13c43974785a29315c80a8b45d52
cdallosb <cristiandaviddallosbustos@gmail.com>
2019-04-11 14:35:04 -0500

    Include aditional operations

--- app.js
+++ app.js
@@ -9,8 +9,53 @@ var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {flag
 app.use(morgan('combined',{stream: accessLogStream}));
 
 var postingAPI = 'https://localhost:8443/';
-    
+
+app.all("/v2/producto/1", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/2", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
 app.all("/v2/producto/3", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/4", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/5", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/6", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/7", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/8", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/9", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/10", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/11", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/12", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/13", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/14", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/15", function(req, res) {
+    apiProxy.web(req, res, {target: postingAPI});    
+});
+app.all("/v2/producto/16", function(req, res) {
     apiProxy.web(req, res, {target: postingAPI});    
 });
 
